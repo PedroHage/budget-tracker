fc1dcf29d1516797118bd8d7369ecd13a431a854
Pedro Lawinscky <pedrohage2008@gmail.com>
2025-06-29 21:43:38 -0300

    feat: Added edit transaction functionality
    fix: category statistics repeating for every element in the category (what was going in my mind when I did that?)

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ while (true)
     Console.WriteLine("[4] View Income");
     Console.WriteLine("[5] View Expenses");
     Console.WriteLine("[6] Filter by Category");
-    Console.WriteLine("[7] Exit");
+    Console.WriteLine("[7] Edit Transaction");
+    Console.WriteLine("[8] Exit");
     Console.Write("> ");
     char userInput = Console.ReadKey().KeyChar;
     Console.WriteLine("\n");
@@ -24,10 +25,10 @@ while (true)
         case '4': Console.WriteLine(GetIncome(transactions)); break; // transactions.Where(t => t.Amount > 0).Sum(t => t.Amount)
         case '5': Console.WriteLine(GetExpenses(transactions)); break;
         case '6': FilterByCategory(); break;
-        case '7': Environment.Exit(0); break;
+        case '7': EditTransaction(); break;
+        case '8': Environment.Exit(0); break;
         default: Console.WriteLine("Invalid option."); break;
     }
-
 }
 
 void AddTransaction()
@@ -50,7 +51,7 @@ void AddTransaction()
     }
     
     userInput = Console.ReadLine();
-    if (!int.TryParse(userInput, out int categoryIndex) || categoryIndex < 0 || categoryIndex > 9 )
+    if (!int.TryParse(userInput, out int categoryIndex) || categoryIndex < 0 || categoryIndex > categories.Length )
     {
         Console.WriteLine("\nInvalid category.");
         return;
@@ -64,7 +65,7 @@ void ViewTransactions(List<Transaction> transactions)
     Console.WriteLine("Transactions:");
     for (int i = 0; i < transactions.Count; i++)
     {
-        Console.WriteLine($"\n{i}. Amount: ${transactions[i].Amount}\nCategory: {transactions[i].Category}\nDescription: {transactions[i].Description}\nDate: {transactions[i].Date}");
+        Console.WriteLine($"\n{i+1}. Amount: ${transactions[i].Amount}\nCategory: {transactions[i].Category}\nDescription: {transactions[i].Description}\nDate: {transactions[i].Date}");
     }
 }
 
@@ -92,7 +93,65 @@ void FilterByCategory()
         if (filteredTransactions.Count > 0)
         {
             Console.WriteLine($"{category}: ");
-            filteredTransactions.ForEach(t => Console.WriteLine($"{GetIncome(filteredTransactions)}\n{GetExpenses(filteredTransactions)}\n{GetBalance(filteredTransactions)}\n"));
+            Console.WriteLine($"{GetIncome(filteredTransactions)}\n{GetExpenses(filteredTransactions)}\n{GetBalance(filteredTransactions)}\n");
         }
     }
+}
+
+void EditTransaction()
+{
+    ViewTransactions(transactions);
+    Console.Write("Choose the transaction to edit:");
+    var transactionChoice = Console.ReadLine();
+    Transaction chosenTransaction;
+    if (int.TryParse(transactionChoice, out int transactionIndex) && transactions.ElementAtOrDefault(transactionIndex - 1) != null)
+    {
+        chosenTransaction = transactions[transactionIndex - 1];
+    }
+    else
+    {
+        Console.WriteLine("\nNo such transaction.");
+        return;
+    }
+
+    Console.WriteLine("Leave blank to maintain previous info.\n");
+    Console.Write("Amount: ");
+    var editedAmount = Console.ReadLine();
+    if (decimal.TryParse(editedAmount, out decimal amount))
+    {
+        chosenTransaction.Amount = amount;
+        service.SaveTransactions(transactions);
+    }
+    else if (!string.IsNullOrEmpty(editedAmount))
+    {
+        Console.WriteLine("Invalid Amount.");
+        return;
+    }
+    
+    Console.Write("\nDescription: ");
+    var editedDescription = Console.ReadLine();
+    if (!string.IsNullOrEmpty(editedDescription))
+    {
+        chosenTransaction.Description = editedDescription;
+        service.SaveTransactions(transactions);   
+    }
+
+    Console.WriteLine("\nCategory: \n");
+    for (int i = 0; i < categories.Length; i++)
+    {
+        Console.WriteLine($"[{i}] {categories[i]}");
+    }
+    
+    var editedCategoryChoice = Console.ReadLine();
+    if (int.TryParse(editedCategoryChoice, out int categoryIndex))
+    {
+        chosenTransaction.Category = categories[categoryIndex];
+        service.SaveTransactions(transactions);
+    }
+    else if (!string.IsNullOrEmpty(editedCategoryChoice))
+    {
+        Console.WriteLine("\nInvalid category.");
+        return;
+    }       
+    Console.WriteLine("Transaction edited with success.");
 }
